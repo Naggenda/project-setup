523f1e83cd7feb97bb98f8049f1bd2f0afa00084
Naggenda <naggendajoshua@gmail.com>
2022-09-30 14:50:43 +0300

    add todo button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Constructor } from '@babel/types';
+
+const Button = () => {
+    
+    return (
+        <div>
+            <input type="submit" value="add Todo" className="btn btn-primary" />
+        </div>
+    )
+}
+
+export default Button
